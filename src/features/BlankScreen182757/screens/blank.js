bba6b3b560831c1a9f71b011dd872214ac371968
Crowdbotics <team@crowdbotics.com>
2020-07-22 20:16:04 +0000

    Update components

--- src/features/BlankScreen182757/screens/blank.js
+++ src/features/BlankScreen182757/screens/blank.js
@@ -22,7 +22,7 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { Input_53: "", Input_60: "" }
 
   render = () => (
     <View
@@ -586,7 +586,7 @@ export class _Blank extends React.Component {
                   >
                     <Text
                       style={{
-                        marginLeft: 0,
+                        marginLeft: 5,
                         marginRight: 0,
                         marginTop: 5,
                         marginBottom: 5,
@@ -650,7 +650,36 @@ export class _Blank extends React.Component {
                       borderRadius: 0,
                       backgroundSize: "auto"
                     }}
-                  />
+                  >
+                    <Slider
+                      value={1}
+                      minimumValue={0}
+                      maximumValue={5}
+                      step={1}
+                      disabled={false}
+                      style={{
+                        marginLeft: 0,
+                        marginRight: 0,
+                        marginTop: 5,
+                        marginBottom: 5,
+                        paddingLeft: 5,
+                        paddingRight: 5,
+                        paddingTop: 5,
+                        paddingBottom: 5,
+                        overflow: "visible",
+                        textAlign: "left",
+                        verticalAlign: "baseline",
+                        borderColor: "#fd5f80",
+                        borderStyle: "solid",
+                        borderWidth: 0,
+                        borderLeftWidth: 0,
+                        borderRightWidth: 0,
+                        borderTopWidth: 0,
+                        borderBottomWidth: 0,
+                        borderRadius: 0
+                      }}
+                    />
+                  </View>
                 </View>
               </View>
               <View
@@ -688,7 +717,38 @@ export class _Blank extends React.Component {
                   borderRadius: 0,
                   backgroundSize: "auto"
                 }}
-              />
+              >
+                <Text
+                  style={{
+                    marginLeft: 5,
+                    marginRight: 0,
+                    marginTop: 5,
+                    marginBottom: 5,
+                    paddingLeft: 5,
+                    paddingRight: 5,
+                    paddingTop: 5,
+                    paddingBottom: 5,
+                    overflow: "visible",
+                    textAlign: "left",
+                    verticalAlign: "baseline",
+                    fontSize: 17,
+                    color: "#000000",
+                    backgroundColor: "#ffffff",
+                    fontStyle: "normal",
+                    fontWeight: "600",
+                    borderColor: "#000000",
+                    borderStyle: "solid",
+                    borderWidth: 0,
+                    borderLeftWidth: 0,
+                    borderRightWidth: 0,
+                    borderTopWidth: 0,
+                    borderBottomWidth: 0,
+                    borderRadius: 0
+                  }}
+                >
+                  Review Title
+                </Text>
+              </View>
               <View
                 style={{
                   width: "100%",
@@ -724,7 +784,44 @@ export class _Blank extends React.Component {
                   borderRadius: 0,
                   backgroundSize: "auto"
                 }}
-              />
+              >
+                <Input
+                  placeholder="Title Review Here"
+                  editable={true}
+                  textStyle={{ fontSize: 16, color: "#000000" }}
+                  style={{
+                    width: "100%",
+                    marginLeft: 10,
+                    marginRight: 0,
+                    marginTop: 5,
+                    marginBottom: 5,
+                    paddingLeft: 5,
+                    paddingRight: 5,
+                    paddingTop: 5,
+                    paddingBottom: 5,
+                    overflow: "visible",
+                    textAlign: "left",
+                    verticalAlign: "baseline",
+                    fontSize: 16,
+                    color: "#000000",
+                    backgroundColor: "#ffffff",
+                    fontStyle: "normal",
+                    fontWeight: "normal",
+                    borderColor: "#000000",
+                    borderStyle: "solid",
+                    borderWidth: 0,
+                    borderLeftWidth: 0,
+                    borderRightWidth: 0,
+                    borderTopWidth: 0,
+                    borderBottomWidth: 0,
+                    borderRadius: 0
+                  }}
+                  value={this.state.Input_53}
+                  onChangeText={nextValue =>
+                    this.setState({ Input_53: nextValue })
+                  }
+                />
+              </View>
               <View
                 style={{
                   width: "100%",
@@ -760,7 +857,38 @@ export class _Blank extends React.Component {
                   borderRadius: 0,
                   backgroundSize: "auto"
                 }}
-              />
+              >
+                <Text
+                  style={{
+                    marginLeft: 5,
+                    marginRight: 0,
+                    marginTop: 5,
+                    marginBottom: 5,
+                    paddingLeft: 5,
+                    paddingRight: 5,
+                    paddingTop: 5,
+                    paddingBottom: 5,
+                    overflow: "visible",
+                    textAlign: "left",
+                    verticalAlign: "baseline",
+                    fontSize: 17,
+                    color: "#000000",
+                    backgroundColor: "#ffffff",
+                    fontStyle: "normal",
+                    fontWeight: "600",
+                    borderColor: "#000000",
+                    borderStyle: "solid",
+                    borderWidth: 0,
+                    borderLeftWidth: 0,
+                    borderRightWidth: 0,
+                    borderTopWidth: 0,
+                    borderBottomWidth: 0,
+                    borderRadius: 0
+                  }}
+                >
+                  Item Type
+                </Text>
+              </View>
               <View
                 style={{
                   width: "100%",
@@ -796,7 +924,44 @@ export class _Blank extends React.Component {
                   borderRadius: 0,
                   backgroundSize: "auto"
                 }}
-              />
+              >
+                <Input
+                  placeholder="Sample text input placeholder"
+                  editable={true}
+                  textStyle={{ fontSize: 20, color: "#000000" }}
+                  style={{
+                    width: "100%",
+                    marginLeft: 0,
+                    marginRight: 0,
+                    marginTop: 5,
+                    marginBottom: 5,
+                    paddingLeft: 5,
+                    paddingRight: 5,
+                    paddingTop: 5,
+                    paddingBottom: 5,
+                    overflow: "visible",
+                    textAlign: "left",
+                    verticalAlign: "baseline",
+                    fontSize: 20,
+                    color: "#000000",
+                    backgroundColor: "#ffffff",
+                    fontStyle: "normal",
+                    fontWeight: "normal",
+                    borderColor: "#000000",
+                    borderStyle: "solid",
+                    borderWidth: 0,
+                    borderLeftWidth: 0,
+                    borderRightWidth: 0,
+                    borderTopWidth: 0,
+                    borderBottomWidth: 0,
+                    borderRadius: 0
+                  }}
+                  value={this.state.Input_60}
+                  onChangeText={nextValue =>
+                    this.setState({ Input_60: nextValue })
+                  }
+                />
+              </View>
               <View
                 style={{
                   width: "100%",
