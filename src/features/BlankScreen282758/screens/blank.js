ba27ca374ed509381811f6558f2de941f9f6e534
Crowdbotics <team@crowdbotics.com>
2020-07-22 17:04:29 +0000

    Update components

--- src/features/BlankScreen282758/screens/blank.js
+++ src/features/BlankScreen282758/screens/blank.js
@@ -64,7 +64,7 @@ export class _Blank extends React.Component {
       <View
         style={{
           width: "100%",
-          height: "50%",
+          height: "100%",
           marginLeft: 0,
           marginRight: 0,
           marginTop: 0,
