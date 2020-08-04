607069d2dfc1c862a32f54696f67635a78bee9ca
Crowdbotics <team@crowdbotics.com>
2020-08-04 18:10:57 +0000

    Update components

--- src/features/BlankScreen1284936/screens/blank.js
+++ src/features/BlankScreen1284936/screens/blank.js
@@ -264,6 +264,41 @@ export class _Blank extends React.Component {
             Explore products in all kinds of categories based on what you are
             searching for
           </Text>
+          <Button
+            style={{
+              width: "100%",
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: "10%",
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              textAlign: "center",
+              verticalAlign: "baseline",
+              fontSize: 12,
+              color: "#ffffff",
+              backgroundColor: "#fd6f80",
+              fontFamily: "Helvetica-Regular",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0,
+              textDecorationLine: "none",
+              textTransform: "none",
+              lineHeight: 12,
+              letterSpacing: 0
+            }}
+            onPress={() => alert("Pressed!")}
+          >
+            CONTINUE
+          </Button>
         </View>
       </View>
     </View>
