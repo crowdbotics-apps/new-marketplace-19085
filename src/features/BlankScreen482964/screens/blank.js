9c7221a0fa7fce9ea2b371cc379fabc8d4472ef1
Crowdbotics <team@crowdbotics.com>
2020-08-05 19:42:43 +0000

    Update components

--- src/features/BlankScreen482964/screens/blank.js
+++ src/features/BlankScreen482964/screens/blank.js
@@ -200,7 +200,181 @@ export class _Blank extends React.Component {
             borderRadius: 0,
             backgroundSize: "auto"
           }}
-        />
+        >
+          <View
+            style={{
+              width: "100%",
+              height: "50%",
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 0,
+              marginBottom: 0,
+              paddingLeft: 0,
+              paddingRight: 0,
+              paddingTop: 0,
+              paddingBottom: 0,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              fontSize: 12,
+              color: "#000000",
+              backgroundColor: "#ffffff",
+              fontFamily: "Roboto-Regular",
+              flexDirection: "column",
+              flexWrap: "wrap",
+              justifyContent: "flex-start",
+              alignItems: "stretch",
+              alignContent: "stretch",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0,
+              backgroundSize: "auto"
+            }}
+          >
+            <View
+              style={{
+                width: "100%",
+                height: "50%",
+                marginLeft: 0,
+                marginRight: 0,
+                marginTop: 0,
+                marginBottom: 0,
+                paddingLeft: 0,
+                paddingRight: 0,
+                paddingTop: 0,
+                paddingBottom: 0,
+                overflow: "visible",
+                textAlign: "left",
+                verticalAlign: "baseline",
+                fontSize: 12,
+                color: "#000000",
+                backgroundColor: "#ffffff",
+                fontFamily: "Roboto-Regular",
+                flexDirection: "column",
+                flexWrap: "wrap",
+                justifyContent: "flex-start",
+                alignItems: "stretch",
+                alignContent: "stretch",
+                borderColor: "#000000",
+                borderStyle: "solid",
+                borderWidth: 0,
+                borderLeftWidth: 0,
+                borderRightWidth: 0,
+                borderTopWidth: 0,
+                borderBottomWidth: 0,
+                borderRadius: 0,
+                backgroundSize: "auto"
+              }}
+            >
+              <Text
+                style={{
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginTop: 5,
+                  marginBottom: 5,
+                  paddingLeft: 5,
+                  paddingRight: 5,
+                  paddingTop: 5,
+                  paddingBottom: 5,
+                  overflow: "visible",
+                  textAlign: "center",
+                  verticalAlign: "baseline",
+                  fontSize: 20,
+                  color: "#fd6f80",
+                  backgroundColor: "#ffffff",
+                  fontFamily: "Roboto-Bold",
+                  borderColor: "#000000",
+                  borderStyle: "solid",
+                  borderWidth: 0,
+                  borderLeftWidth: 0,
+                  borderRightWidth: 0,
+                  borderTopWidth: 0,
+                  borderBottomWidth: 0,
+                  borderRadius: 0,
+                  textDecorationLine: "none",
+                  textTransform: "none",
+                  lineHeight: 12,
+                  letterSpacing: 0
+                }}
+              >
+                Offer Accepted
+              </Text>
+              <Text
+                style={{
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginTop: 5,
+                  marginBottom: 5,
+                  paddingLeft: 5,
+                  paddingRight: 5,
+                  paddingTop: 5,
+                  paddingBottom: 5,
+                  overflow: "visible",
+                  textAlign: "left",
+                  verticalAlign: "baseline",
+                  fontSize: 12,
+                  color: "#000000",
+                  backgroundColor: "#ffffff",
+                  fontFamily: "Roboto-Regular",
+                  borderColor: "#000000",
+                  borderStyle: "solid",
+                  borderWidth: 0,
+                  borderLeftWidth: 0,
+                  borderRightWidth: 0,
+                  borderTopWidth: 0,
+                  borderBottomWidth: 0,
+                  borderRadius: 0,
+                  textDecorationLine: "none",
+                  textTransform: "none",
+                  lineHeight: 12,
+                  letterSpacing: 0
+                }}
+              >
+                Sample text content
+              </Text>
+            </View>
+            <View
+              style={{
+                width: "100%",
+                height: "50%",
+                marginLeft: 0,
+                marginRight: 0,
+                marginTop: 0,
+                marginBottom: 0,
+                paddingLeft: 0,
+                paddingRight: 0,
+                paddingTop: 0,
+                paddingBottom: 0,
+                overflow: "visible",
+                textAlign: "left",
+                verticalAlign: "baseline",
+                fontSize: 12,
+                color: "#000000",
+                backgroundColor: "#ffffff",
+                fontFamily: "Roboto-Regular",
+                flexDirection: "column",
+                flexWrap: "wrap",
+                justifyContent: "flex-start",
+                alignItems: "stretch",
+                alignContent: "stretch",
+                borderColor: "#000000",
+                borderStyle: "solid",
+                borderWidth: 0,
+                borderLeftWidth: 0,
+                borderRightWidth: 0,
+                borderTopWidth: 0,
+                borderBottomWidth: 0,
+                borderRadius: 0,
+                backgroundSize: "auto"
+              }}
+            />
+          </View>
+        </View>
         <View
           style={{
             width: "100%",
